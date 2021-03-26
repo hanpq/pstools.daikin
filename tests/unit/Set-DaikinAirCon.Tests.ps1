40322ff978152a5ef935b61a4c001cf13312a607
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-03-26 13:56:45 +0100

    added tests

--- tests/unit/Set-DaikinAirCon.Tests.ps1
+++ tests/unit/Set-DaikinAirCon.Tests.ps1
@@ -8,7 +8,7 @@ BeforeAll {
         }
     }
     Mock -CommandName Invoke-RestMethod {}
-    
+
     function Get-DaikinControlInfo
     {
         $Hostname,
@@ -16,24 +16,79 @@ BeforeAll {
     }
     Mock -CommandName Get-DaikinControlInfo -MockWith {
         [pscustomobject]@{
-            pow    = ''
-            mode   = ''
-            stemp  = ''
-            shum   = ''
-            f_rate = ''
-            f_dir  = ''
+            pow    = $true
+            mode   = 1
+            stemp  = 22
+            shum   = 22
+            f_rate = 'Level_1'
+            f_dir  = 'VerticalSwing'
         }
     }
+    Mock Write-Host -MockWith {}
 }
 
 Describe -Name 'Set-DaikinAirCon.ps1' -Fixture {
     BeforeAll {
     }
     Context -Name 'When setting mode' {
-        BeforeAll {
-        }
         It 'Should not throw' {
             { Set-DaikinAirCon -HostName 'daikin.contoso.com' -Mode Auto } | Should -Not -Throw
         }
     }
+    Context -Name 'When setting Temp' {
+        It 'Should not throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -Temp 22 } | Should -Not -Throw
+        }
+    }
+    Context -Name 'When setting PowerOn' {
+        It 'Should not throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -PowerOn:$true } | Should -Not -Throw
+        }
+    }
+    Context -Name 'When setting FanSpeed' {
+        It 'Should not throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -FanSpeed AUTO } | Should -Not -Throw
+        }
+    }
+    Context -Name 'When setting FanDirection' {
+        It 'Should not throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -FanDirection HorizontalSwing } | Should -Not -Throw
+        }
+    }
+    Context -Name 'When configuration fails with non OK response' {
+        BeforeAll {
+            function Convert-DaikinResponse {}
+            Mock -CommandName Convert-DaikinResponse {
+                [pscustomobject]@{
+                    ret = 'PARAM NG'
+                }
+            }
+        }
+        It 'Should throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -Mode Auto } | Should -Throw
+        }
+    }
+    Context -Name 'When stemp or shum is "--"' {
+        BeforeAll {
+            function Get-DaikinControlInfo
+            {
+                $Hostname,
+                $Raw
+            }
+            Mock -CommandName Get-DaikinControlInfo -MockWith {
+                [pscustomobject]@{
+                    pow    = $true
+                    mode   = 1
+                    stemp  = '--'
+                    shum   = '--'
+                    f_rate = 'Level_1'
+                    f_dir  = 'VerticalSwing'
+                }
+            }
+        }
+        It 'Should throw' {
+            { Set-DaikinAirCon -HostName 'daikin.contoso.com' -Mode Auto } | Should -Not -Throw
+        }
+    }
+
 }
